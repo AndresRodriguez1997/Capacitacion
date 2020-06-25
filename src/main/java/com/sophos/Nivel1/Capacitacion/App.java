09c34d07b0be214c52f8ef814f87f823c4d91e0e
Andres Felipe Rodriguez Silva <andres.rodriguez@sophossolutions.com>
2020-06-24 23:21:16 -0500

    Tarea 1

--- src/main/java/com/sophos/Nivel1/Capacitacion/App.java
+++ src/main/java/com/sophos/Nivel1/Capacitacion/App.java
@@ -1,5 +1,7 @@
 package com.sophos.Nivel1.Capacitacion;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Scanner leer = new Scanner (System.in);
     }
 }
---
+++ src/main/java/com/sophos/Nivel1/Capacitacion/VariablesCondicionales.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.sophos.Nivel1.Capacitacion;
+
+import java.util.Scanner;
+
+/**
+ * @author andres.rodriguez
+ *
+ */
+public class VariablesCondicionales {
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args )
+    {
+        System.out.println( "Programa para saber si una persona es mayor de edad o no" );
+        System.out.println( "Digite un número" );
+        
+        Scanner leer = new Scanner (System.in);
+        
+        int x = leer.nextInt();
+        
+        if (x < 0) {
+        	System.out.println( "Usted ha digita un dato menor que cero" );
+        }else if (x < 18) {
+        	System.out.println( "Usted es menor de edad" );
+        }else 
+        	System.out.println( "Usted es mayor de edad" );
+                
+        System.out.println( "Su edad es:" + x + " años.");
+        
+              
+    }
+	
+	
+public void Variables() {
+		
+		byte a = 1, b = 2;		
+		short c = 3, d = 4;		
+		int e = 3, f = 4;		
+		long g = 5, h = 6;		
+		float i = 7, j = 8;		
+		double k = 9, l = 10;		
+		boolean m = true, n = false;		
+		char o = 14,  p = 12;
+				
+		
+	}
+   
+	
+
+}
